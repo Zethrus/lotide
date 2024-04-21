77eb6a9b9c753d1ff85037c7108b5cf6ce37ad84
Victor Lane <contact@zethr.us>
2024-04-21 12:30:26 -0600

    Simplify our head() function as per feedback suggestion

--- head.js
+++ head.js
@@ -7,11 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
-  if (array.length === 0) {
-    return undefined;
-  } else {
     return array[0];
-  }
 };
 
 assertEqual(head([5, 6, 7]), 5);
