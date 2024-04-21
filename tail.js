5580713332836587f2a44125541ea173497b02c2
Victor Lane <contact@zethr.us>
2024-04-21 12:56:59 -0600

    Simplify our tail() function and add more testing

--- tail.js
+++ tail.js
@@ -8,11 +8,7 @@ const assertEqual = function(actual, expected) {
 
 // Implement our tail function
 const tail = function(array) {
-  if (array.length <= 1) {
-    return [];
-  } else {
-    return array.slice(1);
-  }
+  return array.slice(1);
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
@@ -21,6 +17,8 @@ const tailResult = tail(words);
 assertEqual(tailResult[0], "Lighthouse"); // Compare the first element of the tail array
 assertEqual(tailResult[1], "Labs"); // Compare the second element of the tail array
 assertEqual(tailResult.length, 2); // Ensure the length of the tail array is correct
+assertEqual(tail(["test"]).length, 0);
+assertEqual(tail([]).length, 0);
 
-// Check if the original array is not modified
+// Check the original array was modified
 assertEqual(words.length, 3);
