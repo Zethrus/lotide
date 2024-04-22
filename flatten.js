0d94a14c32f46a1901119685d54ae20c3fb7520f
Victor Lane <contact@zethr.us>
2024-04-21 18:31:17 -0600

    Add our flatten function to lotide

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  // Check if the arrays are the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check element of the arrays using every method instead of iterating with a for loop
+  return arr1.every((element, index) => element === arr2[index]);
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(arr) {
+  const flattenedArray = [];
+
+  // Iterate through each item in the array
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      item.forEach((item) => flattenedArray.push(item));
+    } else {
+      // If the item is not an array, push it to the flattenedArray
+      flattenedArray.push(item);
+    }
+  });
+
+  return flattenedArray;
+};
+
+// Testing
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, [2, 3], 4, [5, 6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, [2], 3, [4], [5, 6]]), [1, 2, 3, 4, 5, 6]);
