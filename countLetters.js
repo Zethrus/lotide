72f79b34a93d1af49279b662fb4616c002a54f92
Victor Lane <contact@zethr.us>
2024-05-07 12:17:48 -0600

    Add countLetters function to lotide

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+  // Loop through characters in the sentence
+  for (const letter of sentence) {
+    // Ignore if character is a space
+    if (letter !== ' ') {
+      letterCount[letter] = (letterCount[letter] || 0) + 1;
+    }
+  }
+  return letterCount;
+};
+
+// Testing
+const result = countLetters("lighthouse in the house");
+const expectedResult = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1
+};
+
+// Check if output result matches the expected result
+for (const letter in expectedResult) {
+  assertEqual(result[letter], expectedResult[letter]);
+}
