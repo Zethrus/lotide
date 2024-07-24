304354dd2e0f485d86ad0f40dc748cc9e17fce2f
Victor Lane <contact@zethr.us>
2024-07-24 07:19:29 -0600

    Move variables within if statement to improve memory handling

--- eqObjects.js
+++ eqObjects.js
@@ -25,10 +25,11 @@ const eqObjects = function (object1, object2) {
   }
 
   for (let key of keys1) {
-    const val1 = object1[key];
-    const val2 = object2[key];
 
     if (Array.isArray(val1) && Array.isArray(val2)) {
+      const val1 = object1[key];
+      const val2 = object2[key];
+      
       if (!eqArrays(val1, val2)) {
         return false;
       }
