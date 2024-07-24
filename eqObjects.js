2775fac862452c6b88f72f166c92a5a0214d7275
Victor Lane <contact@zethr.us>
2024-07-24 07:11:47 -0600

    Implement eqObjects function, still not handling undefined objects correctly

---
+++ eqObjects.js
@@ -0,0 +1,41 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  // Check if the arrays are the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Check element of the arrays using every method instead of iterating with a for loop
+  return arr1.every((element, index) => element === arr2[index]);
+};
+
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    const val1 = object1[key];
+    const val2 = object2[key];
+
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      if (!eqArrays(val1, val2)) {
+        return false;
+      }
+    } else if (val1 !== val2) {
+      return false;
+    }
+  }
+
+  return true;
+};
